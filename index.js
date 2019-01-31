caae628fc074216e704098e80c71ade6e48695b8
Marcus Plenty <marcusplenty@gmail.com>
2019-01-31 21:40:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const questions = [
   {questionText: "Humans can distinguish between over one trillion different smells", answer: true},
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
-
 let question;
+let questionContainer = document.querySelector(".question-container")
+function appendQuestion(quest){
+  let paragraph = document.createElement("p")
+  paragraph.appendChild(quest.questionText)
+  
+  
+}
+}
